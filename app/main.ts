8939df120a8b1d1ec5156b222fc94eb39de80886
Rajat Ahuja <rahuja445@gmail.com>
2024-12-24 22:19:52 -0600

    default

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,11 @@
 import * as net from 'net';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const PORT = 4221;
 const HOST = 'localhost';
 
-// request: GET /echo/abc HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
-// response: HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
+const directory = process.argv[3];
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!');
@@ -22,7 +23,7 @@ const server = net.createServer((socket) => {
   });
 });
 
-const handleIncomingRequest = (request: string) => {
+const handleIncomingRequest = (request: string): string => {
   const requestLine = request.split('\r\n')[0];
   const [method, url, httpVersion] = requestLine.split(' ');
   const hostLine = request.split('\r\n')[1]; // Unused
@@ -37,20 +38,32 @@ const handleIncomingRequest = (request: string) => {
     } else if (url.startsWith('/user-agent')) {
       const userAgent = userAgentLine.split(': ')[1];
       return _formatHttpResponse(200, userAgent);
+    } else if (url.startsWith('/files')) {
+      const file = url.split('/')[2];
+      try {
+        const filePath = path.join(directory, file);
+        const contents = fs.readFileSync(filePath, 'utf-8');
+        return _formatHttpResponse(200, contents, 'application/octet-stream');
+      } catch (err) {
+        return _formatHttpResponse(404);
+      }
     } else {
       return _formatHttpResponse(404);
     }
+  } else {
+    return _formatHttpResponse(500);
   }
 };
 
-const _formatHttpResponse = (status: number, body?: string) => {
+const _formatHttpResponse = (status: number, body?: string, type?: string) => {
+  const contentType = type || 'text/plain';
   let baseResponse = `HTTP/1.1`;
   switch (status) {
     case 200:
       if (!body) {
         return `${baseResponse} 200 OK\r\n\r\n`;
       } else {
-        return `${baseResponse} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}`;
+        return `${baseResponse} 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${body.length}\r\n\r\n${body}`;
       }
     case 404:
       return `${baseResponse} 404 Not Found\r\n\r\n`;

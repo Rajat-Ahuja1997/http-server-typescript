efa9739d4e23f2c46892e8c6156d2b4c9ccd35ab
Rajat Ahuja <rahuja445@gmail.com>
2024-12-24 19:10:10 -0600

    handle 200 / 404

--- app/main.ts
+++ app/main.ts
@@ -9,20 +9,35 @@ console.log('Logs from your program will appear here!');
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    socket.write(_formatHttpResponse(200));
+    const request = data.toString();
+    const response = handleIncomingRequest(request);
+    socket.write(response);
   });
+
   socket.on('close', () => {
     socket.end();
   });
 });
 
+const handleIncomingRequest = (request: string) => {
+  const requestLine = request.split('\r\n')[0];
+  const [method, url, httpVersion] = requestLine.split(' ');
+  if (method === 'GET' && url === '/') {
+    return _formatHttpResponse(200);
+  } else {
+    return _formatHttpResponse(404);
+  }
+};
+
 const _formatHttpResponse = (status: number) => {
-  let baseResponse = `HTTP/1.1 `;
+  let baseResponse = `HTTP/1.1`;
   switch (status) {
     case 200:
-      return `${baseResponse}200 OK\r\n\r\n`;
+      return `${baseResponse} 200 OK\r\n\r\n`;
+    case 404:
+      return `${baseResponse} 404 Not Found\r\n\r\n`;
     default:
-      return `${baseResponse}500 Unhandled Request\r\n\r\n`;
+      return `${baseResponse} 500 Unhandled Request\r\n\r\n`;
   }
 };
 

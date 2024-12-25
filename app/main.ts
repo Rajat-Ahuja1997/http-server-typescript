0e1d155304211b1cda9363fe92ebeca783da6827
Rajat Ahuja <rahuja445@gmail.com>
2024-12-24 18:55:29 -0600

    send 200

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,29 @@
 import * as net from 'net';
 
+const PORT = 4221;
+const HOST = 'localhost';
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    socket.write(_formatHttpResponse(200));
+  });
   socket.on('close', () => {
     socket.end();
   });
 });
 
-server.listen(4221, 'localhost');
+const _formatHttpResponse = (status: number) => {
+  let baseResponse = `HTTP/1.1 `;
+  switch (status) {
+    case 200:
+      return `${baseResponse}200 OK`;
+    default:
+      return `${baseResponse}500 Unhandled Request`;
+  }
+};
+
+server.listen(PORT, HOST);
